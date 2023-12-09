93508f0fcb2f343aec3a8daf49ba348e3ffd42fd
Juster Zhu <zhuzhen723723@163.com>
2023-12-09 23:54:19 +0800

    Update BluetoothProvider.cs

--- src/c#/GeneralLink.Kernel/Bluetooth/BluetoothProvider.cs
+++ src/c#/GeneralLink.Kernel/Bluetooth/BluetoothProvider.cs
@@ -9,24 +9,5 @@ namespace GeneralLink.Kernel.Bluetooth
 {
     internal class BluetoothProvider : ProviderAbstract
     {
-        internal override ProviderAbstract GetResult()
-        {
-            throw new NotImplementedException();
-        }
-
-        internal override ProviderAbstract Launch()
-        {
-            throw new NotImplementedException();
-        }
-
-        internal override Task<ProviderAbstract> LaunchTaskAsync()
-        {
-            throw new NotImplementedException();
-        }
-
-        internal override ProviderAbstract SetParameter<T>(T parameter)
-        {
-            throw new NotImplementedException();
-        }
     }
 }
