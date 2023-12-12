2896c5530eb2b3650f75c3cf6df82b2e7afc82e5
Juster Zhu <zhuzhen723723@163.com>
2023-12-12 23:16:31 +0800

    add bluetooth

---
+++ src/c#/GeneralLink.Kernel/Bluetooth/BluetoothClientProvider.cs
@@ -0,0 +1,37 @@
+﻿using GeneralLink.Kernel.Builder;
+using GeneralLink.Kernel.Provider;
+using InTheHand.Net;
+using InTheHand.Net.Bluetooth;
+using InTheHand.Net.Sockets;
+
+namespace GeneralLink.Kernel.Bluetooth
+{
+    internal class BluetoothClientProvider : ProviderAbstract<BluetoothClientProvider, BuildeParameter>
+    {
+        internal override BluetoothClientProvider GetResult()
+        {
+            return this;
+        }
+
+        internal override BluetoothClientProvider Launch()
+        {
+            BluetoothClient client = new BluetoothClient();
+            BluetoothAddress address = BluetoothAddress.Parse("112233445566");
+            BluetoothEndPoint ep = new BluetoothEndPoint(address, BluetoothService.SerialPort);
+            client.Connect(ep);
+            Stream stream = client.GetStream();
+            stream.Write(new byte[] { 0x01, 0x02, 0x03, 0x04, 0x05 }, 0, 5);
+            return this;
+        }
+
+        internal override Task<BluetoothClientProvider> LaunchTaskAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal override BluetoothClientProvider SetParameter(BuildeParameter parameter)
+        {
+            return this;
+        }
+    }
+}
---
+++ src/c#/GeneralLink.Kernel/Bluetooth/BluetoothServerProvider.cs
@@ -0,0 +1,42 @@
+﻿using GeneralLink.Kernel.Builder;
+using GeneralLink.Kernel.Provider;
+using InTheHand.Net.Bluetooth;
+using InTheHand.Net.Sockets;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneralLink.Kernel.Bluetooth
+{
+
+    internal class BluetoothServerProvider : ProviderAbstract<BluetoothServerProvider, BuildeParameter>
+    {
+        internal override BluetoothServerProvider GetResult()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal override BluetoothServerProvider Launch()
+        {
+            BluetoothListener listener = new BluetoothListener(BluetoothService.SerialPort);
+            listener.Start();
+            BluetoothClient client = listener.AcceptBluetoothClient();
+            Stream stream = client.GetStream();
+            byte[] buffer = new byte[1024];
+            int bytesReceived = stream.Read(buffer, 0, buffer.Length);
+            return this;
+        }
+
+        internal override Task<BluetoothServerProvider> LaunchTaskAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal override BluetoothServerProvider SetParameter(BuildeParameter parameter)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
